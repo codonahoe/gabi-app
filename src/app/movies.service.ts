f103c22bc70eb5af1710163aa8c8de63ba88e4c4
codonahoe <codonahoe@gmail.com>
2023-08-20 09:49:34 -0400

    formatted the search table

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Movie } from './interfaces/movie';
 import { HttpClient } from "@angular/common/http";
 import { text } from 'express';
+import { first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +17,35 @@ export class MoviesService {
   setMovies(){
     this.http.get('assets/FIlm Library.GV.csv', { responseType: 'text' })
       .subscribe(csvData => {
+        this.movies = [];
         let csvToRowArray = csvData.split("\n");
-        console.log(csvToRowArray);
         for(let i = 1; i < csvToRowArray.length - 1; i++){
           let row = csvToRowArray[i].split(",");
+          if(row[2] === ' the"' || row[2] === ' the : Extended"'){
+
+            row[1] = row[1].replace(/"/g,"");
+            row[2] = row[2].replace(/"/g," ");
+
+            const firstLetter = row[2].charAt(1);
+            const firstLetterCap = firstLetter.toUpperCase(); 
+            const word = row[2].slice(2);
+
+            row[2] = firstLetterCap + word;
+            let movie:Movie = {
+              movieNumber: row[0],
+              movieName: row[2] + row[1],
+              date: row[3],
+              genre: row[4],
+              director: row[5],
+              mainCast: row[6],
+              dvdType: row[7],
+              rating: null
+            }
+            if(movie.movieName !== 'Title'){
+              this.movies.push(movie);
+            }
+          }
+          else{
           let movie:Movie = {
             movieNumber: row[0],
             movieName: row[1],
@@ -33,6 +59,7 @@ export class MoviesService {
           if(movie.movieName !== 'Title'){
             this.movies.push(movie);
           }
+        }
         }
         console.log(this.movies)
       })
